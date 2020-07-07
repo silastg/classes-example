0b9a8f6f9596d481c5b07b1047ffb74c1e709e98
dirceuAntunes <dirceu.antunes@garupa.co>
2020-07-06 22:50:02 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+class Pessoa {
+  constructor(nome, altura, peso) {
+    this.nome = nome;
+    this.altura = altura;
+    this.peso = peso;
+  }
+}
+
+class Vingador extends Pessoa {
+  constructor(
+    nome,
+    altura,
+    peso,
+    poderes,
+    temArmadura,
+    temArma,
+    isMembroFundador,
+    isImortal
+  ) {
+    super(nome, altura, peso);
+    this.poderes = poderes;
+    this.temArmadura = temArmadura;
+    this.temArma = temArma;
+    this.isMembroFundador = isMembroFundador;
+    this.isImortal = isImortal;
+  }
+
+  lutar() {
+    alert(`${this.nome} está lutando usando ${this.poderes[1]}`);
+  }
+}
+
+const thor = new Vingador(
+  "Thor",
+  "1.85",
+  "100kg",
+  ["Deus do Trovão", "Super força", "Vôo", "Longevidade"],
+  true,
+  true,
+  false
+);
+
+document.getElementById("lutar").onclick = function () {
+  thor.lutar();
+};
